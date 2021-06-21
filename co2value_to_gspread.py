f7e3dee75c6d2a8f5b99993c0610de64ed07e504
ddaiki5 <62028697+ddaiki5@users.noreply.github.com>
2021-06-21 16:29:41 +0900

    use avarage values

--- co2value_to_gspread.py
+++ co2value_to_gspread.py
@@ -28,19 +28,25 @@ time.sleep(2)
 
 while True:
     x=0
-    while x<1:
+    co2 = []
+    temp = []
+    rh = []
+    while x<10:
         if scd30.get_data_ready():
             m = scd30.read_measurement()
             if m is not None:
-                co2, temp, rh = m[0], m[1], m[2]
+                #co2, temp, rh = m[0], m[1], m[2]
+                co2.append(m[0])
+                temp.append(m[1])
+                rh.append(m[2])
                 #print(f"CO2: {m[0]:.2f}ppm, temp: {m[1]:.2f}'C, rh: {m[2]:.2f}%")
-                x=1
+                x+=1
+            time.sleep(2)
         else:
-            x=0
             time.sleep(0.2)
             
-    worksheet.update_cell(2, 1, co2)
-    worksheet.update_cell(2, 2, temp)
-    worksheet.update_cell(2, 3, rh)
+    worksheet.update_cell(2, 1, sum(co2)/len(co2))
+    worksheet.update_cell(2, 2, sum(temp)/len(temp))
+    worksheet.update_cell(2, 3, sum(rh)/len(rh))
 
     time.sleep(900)
